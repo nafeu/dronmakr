7c2a0d89a1e7c0692cde3192ff67dbf636ede583
Nafeu Nasir <nafeu.nasir@gmail.com>
2026-02-09 12:45:04 -0500

    Add various UI changes to simplify auditionr

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ import eventlet
 
 eventlet.monkey_patch()
 
-import threading
-import subprocess
 import os
 import shutil
 import typer
@@ -56,53 +54,6 @@ def serve_exported_file(filename):
 @app.route("/")
 def index():
     return render_template("index.html", version=__version__)
-
-
-def run_generate(params):
-    """Runs the generate function as a subprocess and updates WebSocket clients."""
-    try:
-        socketio.emit("status", {"message": "generating...", "done": False})
-
-        # Prepare the command
-        cmd = ["python", "dronmakr.py", "generate"]
-        for key, value in params.items():
-            if key in ["shift_octave_down", "shift_root_note"]:
-                cmd.append(f"--{key.replace('_', '-')}")
-            elif value is not None:
-                cmd.append(f"--{key}")
-                cmd.append(str(value))
-
-        # Run the subprocess and capture output
-        process = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-        )
-
-        # Wait for completion
-        process.wait()
-
-        # Send completion message
-        socketio.emit("status", {"message": "generation complete.", "done": True})
-        socketio.emit("exports", {"files": get_latest_exports()})
-
-    except Exception as e:
-        print(f"Error in subprocess: {e}")  # Log error
-        socketio.emit("status", {"message": f"Error: {e}", "done": True})
-
-
-@app.route("/generate", methods=["POST"])
-def generate_route():
-    """Starts the generate process using a subprocess."""
-    try:
-        params = request.get_json() or {}
-
-        # Run the subprocess in a background thread
-        thread = threading.Thread(target=run_generate, args=(params,))
-        thread.start()
-
-        return jsonify({"output": "generator started."}), 202
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 
 @app.route("/skip", methods=["POST"])
