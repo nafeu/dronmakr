b39f7acb556bf0c1e4d664387b82d0eaad6d3d78
Nafeu Nasir <nafeu.nasir@gmail.com>
2026-06-16 22:28:02 -0400

    Fix finicky media playback os issue

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -252,6 +252,7 @@ pub fn run() {
     };
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_macos_fps::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
